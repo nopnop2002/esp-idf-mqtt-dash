bcc8d0256cee795f90e5cd2dce1cdd7080759195
nopnop2002 <>
2026-01-31 11:13:48 +0900

    Changed OLD RMT Driver to NEW RMT Driver

--- ir-control/main/mqtt.c
+++ ir-control/main/mqtt.c
@@ -1,10 +1,11 @@
-/* MQTT (over TCP) Example
+/*
+	MQTT (over TCP) Example
 
-	 This example code is in the Public Domain (or CC0 licensed, at your option.)
+	This example code is in the Public Domain (or CC0 licensed, at your option.)
 
-	 Unless required by applicable law or agreed to in writing, this
-	 software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-	 CONDITIONS OF ANY KIND, either express or implied.
+	Unless required by applicable law or agreed to in writing, this
+	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+	CONDITIONS OF ANY KIND, either express or implied.
 */
 
 #include <stdio.h>
@@ -17,29 +18,23 @@
 #include "esp_event.h"
 #include "esp_mac.h"
 #include "mqtt_client.h"
-#include "driver/rmt.h"
 
 #include "mqtt.h"
-#include "ir_tools.h"
 
-#define RMT_TX_CHANNEL 1 /*!< RMT channel for transmitter */
+#include "driver/rmt_tx.h"
+#include "ir_nec_encoder.h"
+
+#define EXAMPLE_IR_RESOLUTION_HZ 1000000 // 1MHz resolution, 1 tick = 1us
+
 //#define RMT_TX_GPIO_NUM GPIO_NUM_12 /*!< GPIO number for transmitter signal */
 #define RMT_TX_GPIO_NUM CONFIG_RMT_TX_GPIO_NUM /*!< GPIO number for transmitter signal */
 
 static const char *TAG = "MQTT";
 
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-#else
-static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
-#endif
 {
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
 	esp_mqtt_event_handle_t event = event_data;
 	MQTT_t *mqttBuf = handler_args;
-#else
-	MQTT_t *mqttBuf = event->user_context;
-#endif
 	ESP_LOGI(TAG, "taskHandle=0x%x", (unsigned int)mqttBuf->taskHandle);
 	mqttBuf->event_id = event_id;
 	switch (event->event_id) {
@@ -83,9 +78,46 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 			ESP_LOGI(TAG, "Other event id:%d", event->event_id);
 			break;
 	}
-#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
-	return ESP_OK;
-#endif
+}
+
+void initializeRMT(rmt_channel_handle_t *tx_channel, rmt_encoder_handle_t *nec_encoder, rmt_transmit_config_t *transmit_config) {
+	// Setup IR transmitter
+	ESP_LOGI(TAG, "create RMT TX channel");
+	rmt_tx_channel_config_t _tx_channel_cfg = {
+		.clk_src = RMT_CLK_SRC_DEFAULT,
+		.resolution_hz = EXAMPLE_IR_RESOLUTION_HZ,
+		.mem_block_symbols = 64, // amount of RMT symbols that the channel can store at a time
+		.trans_queue_depth = 4,  // number of transactions that allowed to pending in the background, this example won't queue multiple transactions, so queue depth > 1 is sufficient
+		.gpio_num = RMT_TX_GPIO_NUM,
+	};
+	rmt_channel_handle_t _tx_channel = NULL;
+	ESP_ERROR_CHECK(rmt_new_tx_channel(&_tx_channel_cfg, &_tx_channel));
+
+	ESP_LOGI(TAG, "modulate carrier to TX channel");
+	rmt_carrier_config_t _carrier_cfg = {
+		.duty_cycle = 0.33,
+		.frequency_hz = 38000, // 38KHz
+	};
+	ESP_ERROR_CHECK(rmt_apply_carrier(_tx_channel, &_carrier_cfg));
+
+	// this example won't send NEC frames in a loop
+	rmt_transmit_config_t _transmit_config = {
+		.loop_count = 0, // no loop
+	};
+
+	ESP_LOGI(TAG, "install IR NEC encoder");
+	ir_nec_encoder_config_t _nec_encoder_cfg = {
+		.resolution = EXAMPLE_IR_RESOLUTION_HZ,
+	};
+	rmt_encoder_handle_t _nec_encoder = NULL;
+	ESP_ERROR_CHECK(rmt_new_ir_nec_encoder(&_nec_encoder_cfg, &_nec_encoder));
+
+	ESP_LOGI(TAG, "enable RMT TX channels");
+	ESP_ERROR_CHECK(rmt_enable(_tx_channel));
+
+	*tx_channel = _tx_channel;
+	*nec_encoder = _nec_encoder;
+	*transmit_config = _transmit_config;
 }
 
 esp_err_t query_mdns_host(const char * host_name, char *ip);
@@ -106,59 +138,36 @@ void mqtt(void *pvParameters)
 	sprintf(client_id, "esp32-%02x%02x%02x%02x%02x%02x", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
 	ESP_LOGI(TAG, "client_id=[%s]", client_id);
 
-    // Resolve mDNS host name
-    char ip[128];
-    ESP_LOGI(TAG, "CONFIG_MQTT_BROKER=[%s]", CONFIG_MQTT_BROKER);
-    convert_mdns_host(CONFIG_MQTT_BROKER, ip);
-    ESP_LOGI(TAG, "ip=[%s]", ip);
-    char uri[138];
-    sprintf(uri, "mqtt://%s", ip);
-    ESP_LOGI(TAG, "uri=[%s]", uri);
+	// Resolve mDNS host name
+	char ip[128];
+	ESP_LOGI(TAG, "CONFIG_MQTT_BROKER=[%s]", CONFIG_MQTT_BROKER);
+	convert_mdns_host(CONFIG_MQTT_BROKER, ip);
+	ESP_LOGI(TAG, "ip=[%s]", ip);
+	char uri[138];
+	sprintf(uri, "mqtt://%s", ip);
+	ESP_LOGI(TAG, "uri=[%s]", uri);
 
 	MQTT_t mqttBuf;
 	mqttBuf.taskHandle = xTaskGetCurrentTaskHandle();
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
 	esp_mqtt_client_config_t mqtt_cfg = {
 		.broker.address.uri = uri,
 		.broker.address.port = 1883,
 #if CONFIG_BROKER_AUTHENTICATION
-        .credentials.username = CONFIG_AUTHENTICATION_USERNAME,
-        .credentials.authentication.password = CONFIG_AUTHENTICATION_PASSWORD,
+		.credentials.username = CONFIG_AUTHENTICATION_USERNAME,
+		.credentials.authentication.password = CONFIG_AUTHENTICATION_PASSWORD,
 #endif
 		.credentials.client_id = client_id
 	};
-#else
-	esp_mqtt_client_config_t mqtt_cfg = {
-		.user_context = &mqttBuf,
-		.uri = uri,
-		.port = 1883,
-		.event_handle = mqtt_event_handler,
-#if CONFIG_BROKER_AUTHENTICATION
-        .username = CONFIG_AUTHENTICATION_USERNAME,
-        .password = CONFIG_AUTHENTICATION_PASSWORD,
-#endif
-		.client_id = client_id
-	};
-#endif
 
 	esp_mqtt_client_handle_t mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
-
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
 	esp_mqtt_client_register_event(mqtt_client, ESP_EVENT_ANY_ID, mqtt_event_handler, &mqttBuf);
-#endif
-
 	esp_mqtt_client_start(mqtt_client);
 
 	// Setup IR transmitter
-	ESP_LOGI(TAG, "RMT_TX_GPIO_NUM=%d", RMT_TX_GPIO_NUM);
-	rmt_config_t rmt_tx_config = RMT_DEFAULT_CONFIG_TX(RMT_TX_GPIO_NUM, RMT_TX_CHANNEL);
-	rmt_tx_config.tx_config.carrier_en = true;
-	rmt_config(&rmt_tx_config);
-	rmt_driver_install(RMT_TX_CHANNEL, 0, 0);
-	ir_builder_config_t ir_builder_config = IR_BUILDER_DEFAULT_CONFIG((ir_dev_t)RMT_TX_CHANNEL);
-	ir_builder_config.flags |= IR_TOOLS_FLAGS_PROTO_EXT;
-	ir_builder_t *ir_builder = NULL;
-	ir_builder = ir_builder_rmt_new_nec(&ir_builder_config);
+	rmt_channel_handle_t tx_channel = NULL;
+	rmt_encoder_handle_t nec_encoder = NULL;
+	rmt_transmit_config_t transmit_config = {};
+	initializeRMT(&tx_channel, &nec_encoder, &transmit_config);
 	ESP_LOGI(TAG, "Setup IR transmitter done");
 
 	while (1) {
@@ -200,18 +209,12 @@ void mqtt(void *pvParameters)
 			ESP_LOGI(TAG, "cmd=0x%x",cmd);
 			ESP_LOGI(TAG, "addr=0x%x",addr);
 
-			// Send new key code
-			rmt_item32_t *items = NULL;
-			size_t length = 0;
-			ESP_ERROR_CHECK(ir_builder->build_frame(ir_builder, addr, cmd));
-			ESP_ERROR_CHECK(ir_builder->get_result(ir_builder, &items, &length));
-			// To send data according to the waveform items.
-			rmt_write_items(RMT_TX_CHANNEL, items, length, false);
-			// Send repeat code
-			vTaskDelay(pdMS_TO_TICKS(ir_builder->repeat_period_ms));
-			ESP_ERROR_CHECK(ir_builder->build_repeat_frame(ir_builder));
-			ESP_ERROR_CHECK(ir_builder->get_result(ir_builder, &items, &length));
-			rmt_write_items(RMT_TX_CHANNEL, items, length, false);
+			// transmit IR NEC packets
+			const ir_nec_scan_code_t scan_code = {
+				.address = addr,
+				.command = cmd,
+			};
+			ESP_ERROR_CHECK(rmt_transmit(tx_channel, nec_encoder, &scan_code, sizeof(scan_code), &transmit_config));
 		} else if (mqttBuf.event_id == MQTT_EVENT_ERROR) {
 			break;
 		}
